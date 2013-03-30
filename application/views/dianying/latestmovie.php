56659c8ac983140e517faace326dfc8063d31b71
xiongjiewu <563977729@qq.com>
2013-03-31 01:41:28 +0800

    sad

--- application/views/dianying/latestmovie.php
+++ application/views/dianying/latestmovie.php
@@ -2,43 +2,69 @@
     <?php if (!empty($movieList)): ?>
         <div class="span3 bs-docs-sidebar">
             <ul class="nav nav-list bs-docs-sidenav dy_bs-docs-sidenav" style="*width: 220px;">
+                <?php $dI = 1;?>
                 <?php foreach($monthArr as $monthKey => $monthVal):?>
-                    <li><a href="#<?php echo $monthVal;?>" title="点击查看影片详情"><i class="icon-chevron-right"></i> <?php echo $monthKey;?></a></li>
+                    <li><a <?php if ($dI == 1):?>class="click"<?php endif;?> name="<?php echo $monthVal;?>" href="#<?php echo $monthVal;?>" title="点击查看<?php echo $monthKey;?>影片"><i class="icon-chevron-right"></i> <?php echo $monthKey;?></a></li>
+                    <?php $dI++;?>
                 <?php endforeach;?>
             </ul>
         </div>
         <div class="dy_total">
             <?php foreach ($movieList as $movieKey => $movieVal): ?>
                 <?php if (empty($movieVal)){continue;}?>
-                <a id="<?php echo $monthArr[$movieKey];?>"></a>
-                <div class="bs-docs-example">
+                <div class="bs-docs-example" id="<?php echo $monthArr[$movieKey];?>">
                     <div class="dy_info_list">
                         <div class="title">
-                            <?php echo $movieKey;?>
+                            <h3>
+                                <?php echo $movieKey;?>
+                            </h3>
                         </div>
                         <ul class="info_list">
-                            <?php $movieListI = 1;?>
-                            <?php $movieListCount = count($movieVal);?>
                             <?php foreach ($movieVal as $mKey => $mVal): ?>
-                                <li class="<?php if ($movieListI == $movieListCount): ?>lastOne<?php endif; ?>">
+                                <li>
                                     <div class="dy_name_l">
                                         <a class="dy_name"
-                                           href="<?php echo get_url("/detail/index/{$mVal['id']}"); ?>"><?php echo $mVal['name'];?></a>
+                                           href="<?php echo get_url("/detail/index/{$mVal['id']}"); ?>">
+                                            <img class="dy_img" src="<?php echo trim(get_config_value("img_base_url"),"/") . $mVal['image'];?>">
+                                        </a>
+                                        <a class="dy_name"
+                                           href="<?php echo get_url("/detail/index/{$mVal['id']}"); ?>">
+                                            <?php echo $mVal['name'];?>
+                                        </a>
                                     </div>
-                                    <?php if (!empty($watchLinkInfo[$mVal['id']])): ?>
-                                        <?php $watchLinkI = 1; ?>
-                                        <?php foreach ($watchLinkInfo[$mVal['id']] as $watchKey => $watchVal): ?>
-                                            <a class="dy_watch" href="<?php echo $watchVal['link']; ?>" target="_blank">观看链接<?php echo $watchLinkI++;?></a>
-                                        <?php endforeach; ?>
-                                    <?php endif;?>
-                                    <?php if (!empty($downLoadLinkInfo[$mVal['id']])): ?>
-                                        <?php $downLinkI = 1; ?>
-                                        <?php foreach ($downLoadLinkInfo[$mVal['id']] as $downKey => $downVal): ?>
-                                            <a class="dy_down" href="<?php echo $downVal['link']; ?>" target="_blank">下载链接<?php echo $downLinkI++;?></a>
-                                        <?php endforeach; ?>
-                                    <?php endif;?>
+                                    <div class="dy_link_down">
+                                        <div class="dy_watch_down">
+                                            <div class="watch_down">观看：</div>
+                                            <div class="watch_down_link">
+                                                <a href="">链接1</a>
+                                                <a href="">链接2</a>
+                                                <a href="">链接3</a>
+                                                <a href="">链接4</a>
+                                            </div>
+                                        </div>
+                                        <div class="dy_watch_down">
+                                            <div class="watch_down">下载：</div>
+                                            <div class="watch_down_link">
+                                                <a href="">链接1</a>
+                                                <a href="">链接2</a>
+                                                <a href="">链接3</a>
+                                                <a href="">链接4</a>
+                                            </div>
+                                        </div>
+                                    </div>
+<!--                                    --><?php //if (!empty($watchLinkInfo[$mVal['id']])): ?>
+<!--                                        --><?php //$watchLinkI = 1; ?>
+<!--                                        --><?php //foreach ($watchLinkInfo[$mVal['id']] as $watchKey => $watchVal): ?>
+<!--                                            <a class="dy_watch" href="--><?php //echo $watchVal['link']; ?><!--" target="_blank">观看链接--><?php //echo $watchLinkI++;?><!--</a>-->
+<!--                                        --><?php //endforeach; ?>
+<!--                                    --><?php //endif;?>
+<!--                                    --><?php //if (!empty($downLoadLinkInfo[$mVal['id']])): ?>
+<!--                                        --><?php //$downLinkI = 1; ?>
+<!--                                        --><?php //foreach ($downLoadLinkInfo[$mVal['id']] as $downKey => $downVal): ?>
+<!--                                            <a class="dy_down" href="--><?php //echo $downVal['link']; ?><!--" target="_blank">下载链接--><?php //echo $downLinkI++;?><!--</a>-->
+<!--                                        --><?php //endforeach; ?>
+<!--                                    --><?php //endif;?>
                                 </li>
-                                <?php $movieListI++; ?>
                             <?php endforeach;?>
                         </ul>
                     </div>
--- js/dianying/latestmovie.js
+++ js/dianying/latestmovie.js
@@ -1,11 +1,51 @@
 (function($){
+    var init = {
+        daoHangDingWei:function() {
+            var dyInfoObj = $("div.dy_total");
+            var daohangObj = $("ul.dy_bs-docs-sidenav");
+            var daohangTop = daohangObj.offset().top,id;
+            console.log(daohangTop);
+            dyInfoObj.find("div.bs-docs-example").each(function(){
+                var dyInfoTop = $(this).offset().top;
+                console.log(dyInfoTop);
+                var dyInfoH = $(this).height();
+                console.log(dyInfoH);
+                if (parseInt(daohangTop) >= (parseInt(dyInfoTop) - 50) && parseInt(daohangTop) <= (parseInt(dyInfoTop) + parseInt(dyInfoH))) {
+                    id = $(this).attr("id");
+                }
+            });
+            daohangObj.find("li a.click").removeClass("click");
+            daohangObj.find("li a[name='"+id+"']").addClass("click");
+        }
+    };
     $(document).ready(function(){
+        init.daoHangDingWei();
         var daohangObj = $("ul.dy_bs-docs-sidenav");
         daohangObj.bind("mouseover",function(){
             $(this).addClass("bs-docs-sidenav-over");
         });
         daohangObj.bind("mouseleave",function(){
             $(this).removeClass("bs-docs-sidenav-over");
+        });
+        daohangObj.find("li a").each(function(){
+            $(this).bind("click",function(){
+                daohangObj.find("li a.click").removeClass("click");
+                $(this).addClass("click");
+            });
+        });
+        var dyInfoLiObj = $("div.dy_total ul li");
+        dyInfoLiObj.each(function(){
+            $(this).bind("mouseover",function(){
+                $(this).addClass("li_over");
+            });
+        });
+        dyInfoLiObj.each(function(){
+            $(this).bind("mouseleave",function(){
+                $(this).removeClass("li_over");
+            });
+        });
+        $(window).bind("scroll", function() {//当滚动条滚动时
+            init.daoHangDingWei();
         });
     });
 })(jQuery);
