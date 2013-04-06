5e18bda85ae3f5baf454290abedb4b81eb91c2f4
xiongjiewu <563977729@qq.com>
2013-04-06 14:26:51 +0800

    asd

--- application/views/dianying/upcomingmovie.php
+++ application/views/dianying/upcomingmovie.php
@@ -1,3 +1,4 @@
+<a class="go_to_top" title="回到顶部"></a>
 <?php if (!empty($movieList)): ?>
     <?php $movieListI = 1; ?>
     <?php foreach ($movieList as $movieKey => $movieVal): ?>
@@ -85,6 +86,16 @@
                     var url = $($(this).find("a").get(0)).attr("href");
                    window.location.href = url;
                });
+            });
+            $(window).bind("scroll", function() {//当滚动条滚动时
+                if ($(window).scrollTop() > 50) {
+                    $("a.go_to_top").show();
+                } else {
+                    $("a.go_to_top").hide();
+                }
+            });
+            $("a.go_to_top").bind("click",function(){
+                $(window).scrollTop(0);
             });
         });
     })(jQuery);
