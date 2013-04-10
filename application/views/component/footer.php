095e9141cfce044de002378787641c0c18989b79
xiongjiewu <563977729@qq.com>
2013-04-10 19:36:29 +0800

    ads

--- application/views/component/footer.php
+++ application/views/component/footer.php
@@ -16,7 +16,7 @@
             <li class="muted">&middot;</li>
             <li><a href="<?php echo get_url("/topic/index/5/");?>">免责声明</a></li>
             <li class="muted">&middot;</li>
-            <li><a href="http://www.miibeian.gov.cn" target="_blank">沪ICP备1301122</a></li>
+            <li><a href="http://www.miitbeian.gov.cn" target="_blank">沪ICP备13011228号</a></li>
         </ul>
     </div>
 </footer>
