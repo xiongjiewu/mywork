e875820436b0d61a14529a68d8f47ab04037cb74
xiongjiewu <563977729@qq.com>
2013-04-22 12:25:36 +0800

    as

--- application/views/component/footer.php
+++ application/views/component/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p>联系邮箱：<a href="mailto:service@dianying8.tv">service@dianying8.tv</a> 联系电话：13917510351 QQ：1595057099</p>
-        <p>Copyright © 2013 www.dianying8.com All Rights Reserved <a href="http://www.miitbeian.gov.cn" target="_blank">沪ICP备13011228号</a></p>
+        <p>Copyright © 2013 www.dianying8.tv All Rights Reserved <a href="http://www.miitbeian.gov.cn" target="_blank">沪ICP备13011228号</a></p>
         <ul class="footer-links">
 	    <li class="muted">&middot;</li>
             <li><a href="<?php echo get_url("/topic/index/3/");?>">关于我们</a></li>
