23f1aba0ff54ebf6fe704492d73b4d3ba5eee546
jie <1>
2013-06-06 21:32:10 +0800

    asd

--- application/views/base2.php
+++ application/views/base2.php
@@ -52,8 +52,8 @@
         var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
         document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F105a468dfb25231597f17f7accf5c0c4' type='text/javascript'%3E%3C/script%3E"));
     </script>
+    <a href="http://webscan.360.cn/index/checkwebsite/url/dianying8.tv"><img border="0" src="http://img.webscan.360.cn/status/pai/hash/d1bf605d9bdec73cb115ad967bac7918"/></a>
 </div>
 <!-- 百度流量统计js end-->
-<a href="http://webscan.360.cn/index/checkwebsite/url/dianying8.tv"><img border="0" src="http://img.webscan.360.cn/status/pai/hash/d1bf605d9bdec73cb115ad967bac7918"/></a>
 </body>
 </html>
