afbf1a3c159a48d34ccfbedcca849572faf004c9
jie <1>
2013-06-08 16:16:27 +0800

    asd

--- application/views/base2.php
+++ application/views/base2.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="description" content="电影最齐全的网站，我们只专于电影，用户想看的就是我们的宗旨。在这里，您可以找到任何您想看的电影，各种超清电影提供中。"/>
     <meta name="keywords" content="<?php echo APF::get_instance()->get_config_value("base_name");?>电影库,最新电影,电影排行榜,<?php echo APF::get_instance()->get_config_value("base_name");?>网"/>
+    <meta property="qc:admins" content="5271500601611670646" />
     <title><?php if ($this->load->get_title()):?><?php echo $this->load->get_title();?><?php else:?>好吧<?php endif;?></title>
     <link rel="Shortcut Icon" href="/images/common/logohao.png" />
     <link href="/css/main/base2.css" rel="stylesheet">
