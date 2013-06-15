b4a90fb54db25ad6aa11d9bd922c030e493cd8c9
jie <1>
2013-06-15 22:24:06 +0800

    找回密码样式修改

--- application/views/member/password.php
+++ application/views/member/password.php
@@ -3,9 +3,9 @@
         <fieldset>
             <legend>更改密码</legend>
             <label>登录帐号</label>
-            <input type="text" name="username" id="username" placeholder="登录帐号">
+            <input type="text" class="text" name="username" id="username" placeholder="登录帐号">
             <label>安全邮箱</label>
-            <input type="text" name="email" id="email" placeholder="安全邮箱">
+            <input type="text" name="email" class="text" id="email" placeholder="安全邮箱">
             <label class="checkbox">
             </label>
             <button type="submit" name="submit" id="submit" class="btn">提交</button>
