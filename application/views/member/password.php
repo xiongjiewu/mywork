7ef81aadb6955d31809840877a75949a013ba6e9
jie <1>
2013-07-08 22:31:43 +0800

    好吧感谢信bug修复

--- application/views/member/password.php
+++ application/views/member/password.php
@@ -2,8 +2,8 @@
     <form method="post" action="" onsubmit="return false;" autocomplete="off">
         <fieldset>
             <legend>更改密码</legend>
-            <label>安全邮箱</label>
-            <input type="text" name="email" class="text" id="email" placeholder="安全邮箱">
+            <label>登录邮箱</label>
+            <input type="text" name="email" class="text" id="email" placeholder="登录邮箱">
             <label class="checkbox">
             </label>
             <button type="submit" name="submit" id="submit" class="btn">提交</button>
